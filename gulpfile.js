11668e95ee45cfd279ff8b87132cfc698260c76d
tianez <saber_tz@163.com>
2016-08-20 10:31:18 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ var webpackConfig = require('./webpack.config')
 /*
  * 编译less样式文件
  */
-gulp.task('less', function () {
+gulp.task('less', function() {
     gulp.src(['./app/less/*.less', '!app/less/*.layout.less'])
         // .pipe(sourcemaps.init())
         .pipe(less())
@@ -44,9 +44,10 @@ gulp.task('less', function () {
             suffix: '-min'
         }))
         .pipe(gulp.dest('dist/css/'))
+        .pipe(connect.reload())
 })
 
-gulp.task('map', function () {
+gulp.task('map', function() {
     gulp.src(['./app/less/*.less', '!app/less/*.layout.less'])
         .pipe(sourcemaps.init())
         .pipe(less())
@@ -57,7 +58,7 @@ gulp.task('map', function () {
 /*
  * 编译sass样式文件
  */
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     gulp.src('app/sass/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
@@ -66,17 +67,18 @@ gulp.task('sass', function () {
             suffix: '.map'
         }))
         .pipe(gulp.dest('dist/css/'))
+        .pipe(connect.reload())
 })
 
 // 合并、压缩js文件
-gulp.task('js', function () {
+gulp.task('js', function() {
     gulp.src(
-        [
-            'app/js/jquery-1.8.3.min.js',
-            'module/swiper/idangerous.swiper.min.js',
-            'module/imgshow/jquery.royalslider.min.js',
-        ]
-    )
+            [
+                'app/js/jquery-1.8.3.min.js',
+                'module/swiper/idangerous.swiper.min.js',
+                'module/imgshow/jquery.royalslider.min.js',
+            ]
+        )
         .pipe(concat('main.js'))
         .pipe(rename({
             suffix: '.min'
@@ -86,38 +88,10 @@ gulp.task('js', function () {
 })
 
 /*
- * 用webpack编译打包样式文件
- */
-gulp.task("webpack", function () {
-    gulp.src('./app/app.js')
-        .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest('.'))
-        .pipe(connect.reload())
-})
-
-/*
- * 替换样式文件中的图片路径
- */
-gulp.task('rev', function () {
-    gulp.src(['rev-manifest.json', 'dist/css/webpack.css'])
-        .pipe(revCollector())
-        // .pipe(gulp.dest('dist/css/'))
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename('webpack-min.css'))
-        .pipe(gulp.dest('dist/css/'))
-})
-
-gulp.task('web', function (cb) {
-    runSequence('webpack', 'rev', cb);
-});
-
-/*
  * html模板文件编译
  */
 var templatereplace = require('./app/html/template.json')
-gulp.task('html', function () {
+gulp.task('html', function() {
     gulp.src(['./app/html/*.html', '!app/html/*.layout.html'])
         .pipe(fileinclude({
             prefix: '@',
@@ -128,18 +102,45 @@ gulp.task('html', function () {
         .pipe(connect.reload())
 })
 
-gulp.task('webserver', function () {
+/*
+ * 用webpack编译打包样式文件
+ */
+gulp.task("webpack", function() {
+    gulp.src('./app/app.js')
+        .pipe(webpack(webpackConfig))
+        .pipe(gulp.dest('.'))
+        .pipe(connect.reload())
+})
+
+/*
+ * 替换样式文件中的图片路径
+ */
+gulp.task('rev', function() {
+    gulp.src(['rev-manifest.json', 'dist/css/webpack.css'])
+        .pipe(revCollector())
+        // .pipe(gulp.dest('dist/css/'))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename('webpack-min.css'))
+        .pipe(gulp.dest('dist/css/'))
+})
+
+gulp.task('web', function(cb) {
+    runSequence('webpack', 'rev', 'html', cb);
+});
+
+gulp.task('webserver', function() {
     connect.server({
         port: 3000,
         livereload: true
     })
 })
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch(['app/less/*.less', 'module/*/*.less'], ['less', 'map'])
     gulp.watch(['app/sass/*.scss'], ['sass'])
     gulp.watch(['app/html/*', 'module/*/*.html'], ['html'])
-    gulp.watch(['app/*.js', 'app/**/*.js', '!app/**/bundle.js', 'app/less/*.less', 'module/*/*.less', 'app/sass/*.scss'], ['web'])
 })
 
 gulp.task('default', [
